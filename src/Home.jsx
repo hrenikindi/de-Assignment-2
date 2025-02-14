cb29c991e2028354e183c6f66106431ae5a4271d
Harshith Renikindi <harshith.renikidi@kalvium.community>
2025-02-14 22:38:46 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Home=()=>{
+    return(
+        <div className='container'>
+            <div className='item'>
+                <h1>Name:Harshith Renikindi</h1>
+                <h2>Contact:9390048916</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
+
+// export default function Home(){
+//     return(
+//         <div className='container'>
+//              <div className='item'>
+//                  <h1>Name:Harshith Renikindi</h1>
+//                  <h2>Contact:9390048916</h2>
+//              </div>
+//          </div>
+//     )
+// }
